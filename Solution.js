78c511d0b27b41faa2d5b6bd51e7852f2af91e96
ZiGoomul <lei536@gmail.com>
2020-04-29 15:38:36 +0300

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,6 +1,9 @@
 // Выводит JSON файл в читаемом виде, очень полезно для отладки кода
-var ss = SpreadsheetApp.openById(spreadSheetId);
-ss.getSheetByName("Debug").getRange(1,1).setValue(JSON.stringify(contents,null, 7))
+function doPost(e) {
+  var contents = JSON.parse(e.postData.contents);
+  var ss = SpreadsheetApp.openById(spreadSheetId);
+  ss.getSheetByName("Debug").getRange(1,1).setValue(JSON.stringify(contents,null, 7))
+}
 
 // Перемешивает значения в массиве. 
 function shuffle() { // Тасование Фишера — Йетса
